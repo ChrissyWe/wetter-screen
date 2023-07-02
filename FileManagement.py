bf0c5daf99be347df7ae26b0b4f0c749e9c96a6a
Chrissy Wenzel <Christina_Regina@hotmail.de>
2023-07-02 11:11:02 +0200

    change access

--- FileManagement.py
+++ FileManagement.py
@@ -5,16 +5,16 @@ from datetime import datetime, timedelta
 
 def create_csv():
     csv = pd.DataFrame(columns = ["Uhrzeit", "Temperatur_aussen", "Temperatur_spruehanlage", "ueber_30_C"])
-    csv.to_csv(f"/wetter-screen/Data/{datetime.today().date()}_Temperatures.csv", sep=";", index = False)
+    csv.to_csv(f"home/buga/wetter-screen/Data/{datetime.today().date()}_Temperatures.csv", sep=";", index = False)
     #csv.to_csv(f"/wetter-screen/Data/Temperatur_{datum}.csv", sep = ";")
 
 def import_values_to_csv(time, temperatureOutside, temperatureCorridor, above30):
-    dataframe = pd.read_csv(f"/wetter-screen/Data/{datetime.today().date()}_Temperatures.csv", sep = ";")
+    dataframe = pd.read_csv(f"home/buga/wetter-screen/Data/{datetime.today().date()}_Temperatures.csv", sep = ";")
     act_index = dataframe.index.max() + 1
     dataframe.loc[act_index, "Uhrzeit"] = time
     dataframe.loc[act_index, "Temperatur_aussen"] = temperatureOutside
     dataframe.loc[act_index, "Temperatur_spruehanlage"] = temperatureCorridor
     dataframe.loc[act_index, "ueber_30_C"] = above30
-    dataframe.to_csv(f"/wetter-screen/Data/{datetime.today().date()}_Temperatures.csv",  sep=";", index = False)
+    dataframe.to_csv(f"home/buga/wetter-screen/Data/{datetime.today().date()}_Temperatures.csv",  sep=";", index = False)
 
 
