1afb10f91d048e1f3653ba50be20b94e1bfcdf43
Chrissy Wenzel <Christina_Regina@hotmail.de>
2023-07-07 13:19:11 +0200

    change sensors change csv

--- FileManagement.py
+++ FileManagement.py
@@ -8,7 +8,7 @@ def create_csv(date):
     if(os.path.exists(f"/home/buga/Data/{date}_Temperatures.csv")):
         return
     else:
-        csv = pd.DataFrame(columns = ["Uhrzeit", "Temperatur_aussen", "Temperatur_spruehanlage", "ueber_30_C"])
+        csv = pd.DataFrame(columns = ["Uhrzeit", "Temperatur_20m", "Temperatur_15m", "ueber_30_C"])
         csv.to_csv(f"/home/buga/Data/{date}_Temperatures.csv", sep=";", index = False)
     #csv.to_csv(f"/wetter-screen/Data/Temperatur_{datum}.csv", sep = ";")
 
@@ -16,8 +16,8 @@ def import_values_to_csv(time, temperatureOutside, temperatureCorridor, above30)
     dataframe = pd.read_csv(f"/home/buga/Data/{datetime.today().date()}_Temperatures.csv", sep = ";")
     act_index = dataframe.index.max() + 1
     dataframe.loc[act_index, "Uhrzeit"] = time
-    dataframe.loc[act_index, "Temperatur_aussen"] = temperatureOutside
-    dataframe.loc[act_index, "Temperatur_spruehanlage"] = temperatureCorridor
+    dataframe.loc[act_index, "Temperatur_20m"] = temperatureOutside
+    dataframe.loc[act_index, "Temperatur_15m"] = temperatureCorridor
     dataframe.loc[act_index, "ueber_30_C"] = above30
     dataframe.to_csv(f"/home/buga/Data/{datetime.today().date()}_Temperatures.csv",  sep=";", index = False)
 
