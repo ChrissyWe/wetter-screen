21acf6635acfbe4eba4839a10a0e2b125442355c
Chrissy Wenzel <Christina_Regina@hotmail.de>
2023-08-03 11:47:38 +0200

    change picture frame

--- main.py
+++ main.py
@@ -801,16 +801,16 @@ my_label.pack()
 player = tkvideo("/home/buga/wetter-screen/pictures/sample.mp4", my_label, loop=1, size=(1280, 720))
 player.play()
 
-canvas = tk.Canvas(videoFrame, width=int(2000/2), height=int(1125/2), bg='#F0F8FF', highlightthickness=0)
+# Frame Information
+informationFrame = tk.Frame(text_frame, bg="#F0F8FF")
+informationFrame.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+
+canvas = tk.Canvas(informationFrame, width=int(2000/2), height=int(1125/2), bg='#F0F8FF', highlightthickness=0)
 canvas.pack(side="top")
 
 logoImage = tk.PhotoImage(file="/home/buga/wetter-screen/pictures/Logos.png")
 logoImage = logoImage.subsample(2)  # Resize 1/2
 canvas.create_image(10, 20, anchor="nw", image=logoImage)
-
-# Frame Information
-informationFrame = tk.Frame(text_frame, bg="#F0F8FF")
-informationFrame.pack(side="top", fill="both", expand=True, padx=10, pady=10)
 
 #--- Right Frame ---#
 rightFrame = tk.Frame(root, bg="white")
