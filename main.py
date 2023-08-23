f5b7a88437e57f35106b580561420d7cc4c27882
Chrissy Wenzel <Christina_Regina@hotmail.de>
2023-08-23 16:29:22 +0200

    change heatHours

--- main.py
+++ main.py
@@ -699,7 +699,7 @@ humidity = []
 humiditySecond = []
 temperatures = []
 temperaturesSecond = []
-minutesOverThirty = 0
+minutesOverThirty = 144
 
 for item in dataWindSensorFirstWeek:
     if item["deviceId"] == '0004A30B00F7DA67':
